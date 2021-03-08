a8c5306dc58f935bdb2f94795e7078a5cd8a7ede
Dustin Fox <dustinfox1996@gmail.com>
2021-03-08 15:22:34 -0500

    .

---
+++ HomeWorkProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeWorkProject {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            var testTest = "tester";
+            Console.WriteLine(testTest);
+            var dogName = "Willow";
+            var catName = "tigger";
+            var housePets = dogName + " " + catName;
+            Console.WriteLine(housePets);
+            housePets = $" the dogs name is {dogName} the cats name is {catName}";
+            Console.WriteLine(housePets);
+
+            var topFiveStrengths = new string[5];
+                    topFiveStrengths[0] = "Deliberative";
+                    topFiveStrengths[1] = "Context";
+                    topFiveStrengths[2] = "Analytical";
+                    topFiveStrengths[3] = "Harmony";
+                    topFiveStrengths[4] = "Adaptability";
+            Console.WriteLine(topFiveStrengths[1]);
+
+        }
+    }
+}
